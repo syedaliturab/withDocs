0498daafec91f094dbbab0be3304e1428c9ab8bb
RadheTians <radheramantiwari87@gmail.com>
2020-07-30 19:44:46 +0530

    user Featue Update

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,27 +36,33 @@ const createSendToken = (user, statusCode, res) => {
 
 exports.signup = catchAsync(async (req, res, next) => {
 
-  const newUser = await User.create({
-    email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm
-  });
+  const newUser = await User.create(req.body);
   
   createSendToken(newUser, 200, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
-  const { email, password } = req.body;
 
+  var userId;
+  var message;
+
+  if(req.body.email) {
+    userId = {email: req.body.email};
+    message = "email";
+  } else {
+    userId = {contactNo: req.body.contactNo};
+    message = "contact number";
+  }
+  const password = req.body.password;
   // 1) Check if email and password exist
-  if (!email || !password) {
-    return next(new AppError('Please provide email and password!', 400));
+  if (!userId || !password) {
+    return next(new AppError('Please provide '+ message + ' and password!', 400));
   }
   // 2) Check if user exists && password is correct
-  const user = await User.findOne({ email }).select('+password');
+  const user = await User.findOne(userId).select('+password');
 
   if (!user || !(await user.correctPassword(password, user.password))) {
-    return next(new AppError('Incorrect email or password', 401));
+    return next(new AppError('Incorrect '+ message +' or password', 401));
   }
 
   // 3) If everything ok, send token to client
