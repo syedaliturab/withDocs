f7254f0d0a68f673e20498df148ca2b749d0ec81
RadheTians <radheramantiwari87@gmail.com>
2021-02-22 00:39:47 +0530

    woking hours completed

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -2,6 +2,30 @@ const appointments = require('./../models/appointmentModel.js');
 const doctors = require('./../models/doctorModel.js');
 const catchAsynsc = require('./../utils/catchAsync');
 
+const weekday = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+const listSlots = { "00:00-00:30":1,"00:30-01:00":2,"01:00-01:30":3,"01:30-02:00":4,"02:00-02:30":5,
+                    "02:30-03:00":6,"03:00-03:30":7,"03:30-4:00":8,"04:00-04:30":9,"04:30-05:00":10,
+                    "05:00-05:30":11,"05:30-06:00":12,"06:00-06:30":13,"06:30-07:00":14,"07:00-07:30":15,
+                    "07:30-08:00":16,"08:00-08:30":17,"08:30-09:00":18,"09:00-09:30":19,"09:30-10:00":20,
+                    "10:00-10:30":21,"10:30-11:00":22,"11:00-11:30":23,"11:30-12:00":24,"12:00-12:30":25,
+                    "12:30-13:00":26,"13:00-13:30":27,"13:30-14:00":28,"14:00-14:30":29,"14:30-15:00":30,
+                    "15:00-15:30":31,"15:30-16:00":32,"16:00-16:30":33,"16:30-17:00":34,"17:00-17:30":35,
+                    "17:30-18:00":36,"18:00-18:30":37,"18:30-19:00":38,"19:00-19:30":39,"19:30-20:00":40,
+                    "20:00-20:30":41,"20:30-21:00":42,"21:00-21:30":43,"21:30-22:00":44,"22:00-22:30":45,
+                    "22:30-23:00":46,"23:00-23:30":47,"23:30-24:00":48
+                }
+
+const searchIndex = (date,appointment) => {
+    var i = 0;
+    for( var element of appointment){
+        if(element.date == date){
+            return i;
+        }
+        i++;
+    }
+    return -1;
+}
+
 
 const addMinutes = (time, minutes) => {
     var date = new Date(new Date('01/01/2021 ' + time).getTime() + minutes * 60000);
@@ -245,58 +269,6 @@ exports.getWorkingHoursByDoctorId = catchAsynsc(
     }
 );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const weekday = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
-const listSlots = { "00:00-00:30":1,"00:30-01:00":2,"01:00-01:30":3,"01:30-02:00":4,"02:00-02:30":5,
-                    "02:30-03:00":6,"03:00-03:30":7,"03:30-4:00":8,"04:00-04:30":9,"04:30-05:00":10,
-                    "05:00-05:30":11,"05:30-06:00":12,"06:00-06:30":13,"06:30-07:00":14,"07:00-07:30":15,
-                    "07:30-08:00":16,"08:00-08:30":17,"08:30-09:00":18,"09:00-09:30":19,"09:30-10:00":20,
-                    "10:00-10:30":21,"10:30-11:00":22,"11:00-11:30":23,"11:30-12:00":24,"12:00-12:30":25,
-                    "12:30-13:00":26,"13:00-13:30":27,"13:30-14:00":28,"14:00-14:30":29,"14:30-15:00":30,
-                    "15:00-15:30":31,"15:30-16:00":32,"16:00-16:30":33,"16:30-17:00":34,"17:00-17:30":35,
-                    "17:30-18:00":36,"18:00-18:30":37,"18:30-19:00":38,"19:00-19:30":39,"19:30-20:00":40,
-                    "20:00-20:30":41,"20:30-21:00":42,"21:00-21:30":43,"21:30-22:00":44,"22:00-22:30":45,
-                    "22:30-23:00":46,"23:00-23:30":47,"23:30-24:00":48
-                }
-
-const searchIndex = (date,appointment) => {
-    var i = 0;
-    for( var element of appointment){
-        if(element.date == date){
-            return i;
-        }
-        i++;
-    }
-    return -1;
-}
-
 //to get All appointments by doctorId
 exports.getAppointmentsByDoctorId = catchAsynsc(
     async (req, res, next) => {
@@ -329,6 +301,36 @@ exports.bookAppointment = catchAsynsc(
             data: appointment
         });
     
+    }
+);
+
+//to book appointment into clinic details
+exports.bookAppointment = catchAsynsc(
+    async (req, res, next) => {
+        
+        const appointment = await appointments.create(req.body);
+        res.status(200).json({
+            status: 'success',
+            data: appointment
+        });
+    
+    }
+);
+
+//to cancel appointment into clinic details
+exports.cancelAppointment = catchAsynsc(
+    async (req, res, next) => {
+        
+        const updateClinic = await appointments.findByIdAndUpdate(
+            req.body.id, req.body,{
+                new: true,
+                runValidators: true
+            }
+        );
+        res.status(200).json({
+            status: 'success',
+            data: updateClinic
+        });
     }
 );
 
@@ -574,24 +576,6 @@ exports.completeClinicTwoAppointment = catchAsynsc(
         res.status(200).json({
             status: 'success',
             data: appointment
-        });
-    }
-);
-
-//to cancel appointment into clinic details
-exports.cancelAppointment = catchAsynsc(
-    async (req, res, next) => {
-        
-        const updateClinic = await appointments.updateOne(
-            {id:req.params.id},
-            req.body,{
-                new: true,
-                runValidators: true
-            }
-        );
-        res.status(200).json({
-            status: 'success',
-            data: updateClinic
         });
     }
 );
