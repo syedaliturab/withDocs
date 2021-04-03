0e88b0f009afe534d4b43608ed9975b06970c92a
Vedpalvaidya <vedpal.21810673@viit.ac.in>
2021-04-03 19:22:25 +0530

    changes in relative

--- models/patientModel.js
+++ models/patientModel.js
@@ -134,31 +134,13 @@ const patientSchema = mongoose.Schema({
     
 });
 
-const patientRelativeSchema = new mongoose.Schema({
-    relativeId: {
-        type: String,
-	    unique: true,
-        requried: [true, 'Please provide valid id']
-    },
-    patientId : {
-        type: String,
-	    unique: true,
-        requried: [true, 'Please provide valid id']
-    },
-    relationship : {
-        type : String
-    },
-    
-    status : {
-        type : String
-    },
-    occupation : {
-        type : String
-    }
-});
-
 // const patientRelativeSchema = new mongoose.Schema({
-//     relativeId : {
+//     relativeId: {
+//         type: String,
+// 	    unique: true,
+//         requried: [true, 'Please provide valid id']
+//     },
+//     patientId : {
 //         type: String,
 // 	    unique: true,
 //         requried: [true, 'Please provide valid id']
@@ -166,19 +148,7 @@ const patientRelativeSchema = new mongoose.Schema({
 //     relationship : {
 //         type : String
 //     },
-//     name : {
-//         type : String
-//     },
-//     dateOfBirth : {
-//         type : String
-//     },
-//     age: {
-//         type : Number
-//     },
-//     image : String,
-//     contactNo : {
-//         type :Number
-//     },
+    
 //     status : {
 //         type : String
 //     },
@@ -186,6 +156,41 @@ const patientRelativeSchema = new mongoose.Schema({
 //         type : String
 //     }
 // });
+
+const patientRelativeSchema = new mongoose.Schema({
+    relativeId : {
+        type: String,
+	    unique: true,
+        requried: [true, 'Please provide valid id']
+    },
+    patientId : {
+                type: String,
+        	    unique: true,
+                requried: [true, 'Please provide valid id']
+            },
+    relationship : {
+        type : String
+    },
+    name : {
+        type : String
+    },
+    dateOfBirth : {
+        type : String
+    },
+    age: {
+        type : Number
+    },
+    image : String,
+    contactNo : {
+        type :Number
+    },
+    status : {
+        type : String
+    },
+    occupation : {
+        type : String
+    }
+});
 
 const patientRelative = mongoose.model('patientRelative', patientRelativeSchema);
 const patient = mongoose.model('Patient',patientSchema);
