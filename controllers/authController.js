eb6e3dd896ee26177b572aff6e1fbc8730818c86
RadheTians <radheramantiwari87@gmail.com>
2021-04-17 23:29:25 +0530

    Password Updated issues

--- controllers/authController.js
+++ controllers/authController.js
@@ -250,7 +250,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
   // 1) Get user from collection
-  const user = await User.findById(req.user.id).select('+password');
+  const user = await User.findById(req.body.id).select('+password');
 
   // 2) Check if POSTed current password is correct
   if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
